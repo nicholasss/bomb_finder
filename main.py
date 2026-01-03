d0434f6938e23fbd0cd0a444a2a1d1eb464c05c4
Nicholas <nicholasss@users.noreply.github.com>
2026-01-02 22:55:11 -0500

    renamed to pg

--- main.py
+++ main.py
@@ -1,39 +1,42 @@
-import pygame
+import pygame as pg
 
 # local files
 from tileset import Tileset
 
-
+# GLOBAL
 SCREEN_SIZE = 800, 640
 CENTER = SCREEN_SIZE[0] / 2, SCREEN_SIZE[1] / 2
 NAME = "Bomb Finder"
 
+# TILES
+TILE_SCALE = 4
 
 def main():
     tileset_path = "./assets/basic-tileset.png"
-    tileset = pygame.image.load(tileset_path)
-    scaled_tileset = pygame.transform.scale(tileset, (160 * 4, 96 * 4))
+    tileset = pg.image.load(tileset_path)
 
-    pygame.init()
-    screen = pygame.display.set_mode(SCREEN_SIZE)
-    pygame.display.set_caption(NAME)
+    scaled_tileset = pg.transform.scale(tileset, (160 * 4, 96 * 4))
+
+    pg.init()
+    screen = pg.display.set_mode(SCREEN_SIZE)
+    pg.display.set_caption(NAME)
 
     scaled_tileset_rect = scaled_tileset.get_rect(center=CENTER)
 
     screen.blit(scaled_tileset, scaled_tileset_rect)
-    pygame.display.update()
+    pg.display.update()
 
     running = True
     while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
                 running = False
 
-            if event.type == pygame.MOUSEBUTTONUP:
+            if event.type == pg.MOUSEBUTTONUP:
                 print("Clicked!")
 
     # exiting event loop
-    pygame.quit()
+    pg.quit()
 
 
 if __name__ == "__main__":
