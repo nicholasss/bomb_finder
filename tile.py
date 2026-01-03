cb7231f8f7323497494b1c9102e14a50aa694b23
Nicholas <nicholasss@users.noreply.github.com>
2026-01-03 00:32:29 -0500

    tile object added

---
+++ tile.py
@@ -0,0 +1,11 @@
+import pygame as pg
+
+class Tile():
+    def __init__(self, is_empty=False):
+        # manage tile state and change tile image on demand
+        self.__clicked = False
+        self.__is_empty = is_empty
+
+    def click(self) -> bool:
+        self.__clicked = True
+        return self.__is_empty
