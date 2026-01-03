440a96d9517fb3269e9db09ea2033cfbd1208ee4
Nicholas <nicholasss@users.noreply.github.com>
2026-01-02 22:54:38 -0500

    adding processing to tileset

--- tileset.py
+++ tileset.py
@@ -1,16 +1,22 @@
-import pygame
-
+import pygame as pg
 
 class Tileset:
     def __init__(self, path, tile_size=(16, 16)):
         self.path = path
         self.tile_size = tile_size
-
-        self.image = pygame.image.load(self.path)
-        self.rect = self.image.get_rect()
-
         self.tiles = []
+
+        # processing
+        self.image = pg.image.load(self.path)
+        self.rect = self.image.get_rect()
+        self.og_height_px = self.image.height
+        self.og_width_px = self.image.width
+
+        # calling methods
         self.make_tiles()
+
+    def scale_by(self, amount=4):
+        self.tiles = list(map(lambda x: pg.transform.scale(x, amount)))
 
     def make_tiles(self):
         self.tiles = []
@@ -19,6 +25,6 @@ class Tileset:
 
         for x in range(0, self.rect.width, tile_width):
             for y in range(0, self.rect.height, tile_height):
-                tile = pygame.Surface(self.size)
+                tile = pg.Surface(self.size)
                 tile.blit(self.image, (0, 0), (x, y, *self.size))
                 self.tiles.append(tile)
