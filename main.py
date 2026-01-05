313c49d3f6c9e82868eecbc58816eb7890af324a
Nicholas <nicholasss@users.noreply.github.com>
2026-01-05 11:18:31 -0500

    added comments, clock, and more constants

--- main.py
+++ main.py
@@ -2,29 +2,38 @@ import pygame as pg
 
 # local files
 from tileset import Tileset, TileType
+from game import Game
 
 # GLOBAL
 SCREEN_SIZE = 800, 640
 CENTER = SCREEN_SIZE[0] / 2, SCREEN_SIZE[1] / 2
 NAME = "Bomb Finder"
 
-# TILES
+# TILESET
 TILE_SCALE = 4
 TILE_SIZE = (16, 16)
 TILE_PATH = 'assets/basic-tileset.png'
 
+# GAME
+SCREEN_TILE = (TILE_SIZE[0] * TILE_SCALE, TILE_SIZE[1] * TILE_SCALE)
+FPS = 60
+
 def main():
-    # initialization
     pg.init()
     screen = pg.display.set_mode(SCREEN_SIZE)
     pg.display.set_caption(NAME)
-
-    tileset = Tileset(TILE_PATH, TILE_SIZE, TILE_SCALE)
-
-    unlicked_tile = tileset.get_tile(TileType.UNCLICKED)
-    screen.blit(unlicked_tile, screen.get_rect())
+    clock = pg.time.Clock()
     pg.display.update()
 
+    tileset = Tileset(TILE_PATH, TILE_SIZE, TILE_SCALE)
+    game = Game(tileset, SCREEN_TILE, screen)
+    pg.display.update()
+        
+    # create new game screen
+
+    # select difficulty or mode
+
+    # load the tile types
     running = True
     while running:
         for event in pg.event.get():
@@ -32,9 +41,11 @@ def main():
                 running = False
 
             if event.type == pg.MOUSEBUTTONUP:
-                print("Clicked!")
+                print("Click at", event.pos)
 
-    # exiting event loop
+        clock.tick(FPS)
+
+    # exiting event loop to exit
     pg.quit()
 
 
