8d32d2a40d3703a1eb1bcc4102a78ede8b241390
Nicholas <nicholasss@users.noreply.github.com>
2026-01-05 12:36:16 -0500

    updated init method

--- game.py
+++ game.py
@@ -5,30 +5,35 @@ from tile import Tile
 
 class Game:
     def __init__(
-        self, tileset: Tileset, tile_size: tuple[int, int], screen: pg.Surface
+        self,
+        tileset: Tileset,
+        tile_size: tuple[int, int],
+        screen: pg.Surface,
+        seed: str,
     ):
+        """A game instance should returned a fully setup game, ready to play."""
         self.__tileset: Tileset = tileset
         self.__tile_size: tuple[int, int] = tile_size
         self.__screen: pg.Surface = screen
+        self.__seed: str = seed
 
-        # Tile surfaces
-        self.__unclicked_tile = tileset.get_tile(TileType.UNCLICKED).copy()
-
+        # create grid
         self.grid = [[int]]
         for i in range(5):
             for j in range(5):
                 tile_location = [self.__tile_size[0] * i, self.__tile_size[1] * j]
-                new_tile = Tile()
-                self.__screen.blit(self.__unclicked_tile, tile_location)
+
+                # TODO:
+                # new_tile = Tile()
+                # use tile class instead of just tile image
+
+                self.__screen.blit(
+                    self.__tileset.get_tile(TileType.UNCLICKED), tile_location
+                )
+
+        # use seed to place bombs
 
         pg.display.flip()
-
-        # draw the map
-        # put in number of bombs into the
-
-        # perform animations if you lose the game
-
-        # show redo game seed/new game
         print("Game Initialized")
 
     def play_round(self):
