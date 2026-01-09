9020dc40c1ed59ee132cafe08bbf948920155543
Nicholas <nicholasss@users.noreply.github.com>
2026-01-09 16:06:49 -0500

    tile type keeps track of its internal state better

--- tile.py
+++ tile.py
@@ -24,6 +24,11 @@ class Tile:
         self.was_clicked = False
 
     def __update_type(self):
+        """
+        Update the tiles 'TileType', at self.__tile_type, to reflect its current internal state.
+        This method is called every render cycle.
+        """
+        # first looking at non-clicked tile states
         # 1. check hidden states and return early if its hidden
         if not self.was_clicked:
             if self.__flag_state == Flag.NO_FLAG:
@@ -37,40 +42,40 @@ class Tile:
 
             return
 
-        # 2. check clicked states, tile is not hidden
-        # 2a. first render bombs
+        # then looking at clicked tile states without bomb
+        # 2. check clicked states without bomb
+        if not self.__has_bomb:
+            # print(
+            #     f"DEBUG: nbombs={self.__neighboring_bombs} new_type={TileType(self.__neighboring_bombs + 1)}"
+            # )
+            self.__tile_type = TileType(self.__neighboring_bombs + 1)
 
-    def perform_left_click(self) -> bool:
-        """Perfroms a left click on the tile. This will first mark the tile as revealed,
-        then will update internal flag state.
-        Finally it will return whether the click was on a bomb."""
+            return
+
+        # 3. clicked states with bomb
+        # set to bomb first frame
+        self.__tile_type = TileType.BOMB_A
+
+    def perform_left_click(self):
+        """
+        Perfroms a left click on the tile.
+        We will first mark the tile as clicked and then update its state.
+        """
         self.was_clicked = True
-        # a tile cannot be flagged if it is revealed
         self.__flag_state = Flag.NO_FLAG
-
-        # update type
         self.__update_type()
 
-        # NOTE: I think the logic for the following two should be in Game class, but unsure of how to provide the 'trigger'
-        if self.__has_bomb:
-            print("NYI: Game over due to clicking on bomb")
-            return True
-
-        if self.__neighboring_bombs == 0:
-            print("NYI: Flood all empty tiles, and surrounding number tiles.")
-
-        # not clicked on bomb
-        return False
-
     def perform_right_click(self):
-        """Perfroms a right click on the tile.
-        This will cycle through the flags."""
+        """
+        Perfroms a right click on the tile.
+        This will cycle through the flags.
+        """
         self.__cycle_flag()
         self.__update_type()
 
-        print(
-            f"DEBUG: tile updated to tile {self.__tile_type.value}: {self.__tile_type.name}"
-        )
+        # print(
+        #     f"DEBUG: tile updated to tile {self.__tile_type.value}: {self.__tile_type.name}"
+        # )
 
     def get_state(self) -> TileType:
         return self.__tile_type
